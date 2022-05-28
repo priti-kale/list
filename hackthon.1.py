93b3f9e232c43d521505da02556e752d983c1180
Priti Kale <106419875+priti-kale@users.noreply.github.com>
2022-05-28 10:19:12 +0530

    Add files via upload

---
+++ hackthon.1.py
@@ -0,0 +1,33 @@
+# import random
+# import string
+# lower=string.ascii_lowercase
+# upper=string.ascii_uppercase
+# digit=string.digits
+# symbol=string.punctuation
+# sum=lower+upper+digit+symbol
+# lenth=int(input("enter the lenth"))
+# b=[]
+# sum=lower+upper+digit+symbol
+# b.append(sum)
+# k=random.sample(sum, lenth)
+# password="".join(k)
+# print(password)
+# a=["priti","kale","gaurav"]
+# i=0
+# b=[]
+# while i<len(a):
+#     j=0
+#     while j<len(a[i]):
+#         random.shuffle(a[i])
+#         print(a[i])
+#         b.append(a[i])
+#     i=i+1
+# print(b)
+a="prii"
+random.shuffle(a)
+print(a)
+
+
+
+# random.shuffle(a)
+# print(a)
